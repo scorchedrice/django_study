c051590360540b62a96500ae8ee835306951e8a6
woongveloper <hoseakim1025@gmail.com>
2024-03-29 17:53:52 +0900

    240329

--- practice_CRUD/cruds/views.py
+++ practice_CRUD/cruds/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CRUDS
 
 # Create your views here.
@@ -21,13 +21,37 @@ def new(request):
     return render(request,'cruds/new.html')
 
 def create(request):
-    print(request.GET)
-    title = request.GET.get('title')
-    content = request.GET.get('content')
-    # request를 받고 이를 DB에 등록하는 과정
+    print(request.POST)
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    # request를 받고 이를 DB에 등록하는 과정 (POST)
     cruds = CRUDS()
     cruds.title = title
     cruds.content = content
     cruds.save()
     
-    return render(request,'cruds/create.html')
+    return redirect('cruds:detail',cruds.pk)
+    # redirect할 주소, 변수값
+
+def delete(request,pk):
+    cruds = CRUDS.objects.get(pk=pk)
+    cruds.delete()
+    return redirect('cruds:index')
+
+def update(request,pk): # edit.html에서 수정한 정보를 DB에 반영하기 위함.
+    cruds = CRUDS.objects.get(pk=pk)
+
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    
+    cruds.title = title
+    cruds.content = content
+    cruds.save()
+    return redirect('cruds:index')
+
+def edit(request,pk):
+    cruds = CRUDS.objects.get(pk=pk)
+    context = {
+        'cruds':cruds,
+    }
+    return render(request,'cruds/edit.html',context)
